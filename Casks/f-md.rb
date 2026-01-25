36cfbf51c9f0dd836c8a96d8117170e1b2786878
Faisal Mirza <mail@faisalmirza.nlm>
2026-01-25 23:11:06 +0100

    Rename cask from efemde to f-md

--- Casks/f-md.rb
+++ Casks/f-md.rb
@@ -1,15 +1,15 @@
-cask "efemde" do
+cask "f-md" do
   version "1.0.0"
   sha256 "b2cff3daae91bd16423d637d364a4525ac25bc2151eda6c953f1ee4a8fd02381"
 
-  url "https://github.com/faisalmirza/efemde/releases/download/v#{version}/efemde-1.0-macos.zip"
+  url "https://github.com/faisalmirza/f-md/releases/download/v#{version}/F-MD-#{version}-macos.zip"
   name "F-MD"
   desc "Fast markdown viewer + editor"
-  homepage "https://github.com/faisalmirza/efemde"
+  homepage "https://github.com/faisalmirza/f-md"
 
   depends_on macos: ">= :ventura"
 
-  app "efemde.app"
+  app "F-MD.app"
 
   zap trash: [
     "~/Library/Preferences/com.faisalmirza.efemde.plist",
